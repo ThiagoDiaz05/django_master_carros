b2131132dded1d5659b7da4900599fc43dd54ea6
Thiago Diaz <tdiazmateus@gmail.com>
2025-10-08 05:51:38 -0300

    config do admin e models

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from cars.models import Car, Brand
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model','brand','factory_year','model_year','value')
+    search_fields = ('model',)
 
-# Register your models here.
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('model',)
+    
+admin.site.register(Brand, BrandAdmin) 
+admin.site.register(Car, CarAdmin)
+
