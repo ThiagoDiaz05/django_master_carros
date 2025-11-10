f52f37067e1cc16b57472f7a8489e2dbd0988238
Thiago Diaz <tdiazmateus@gmail.com>
2025-11-10 12:06:56 -0300

    add cadastro de carros

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from cars.models import Car
+from cars.forms import CarForm
+def cars_view(request):
+    cars = Car.objects.all().order_by('model')
+    search = request.GET.get('search')
 
-# Create your views here.
+    if search:
+        cars = Car.filter(model__icontains = search).order_by('model')
+    
+        
+    return render(request, 'cars.html', {'cars' : cars})
+
+def new_car_view(request):
+    if request.method == 'POST':
+        new_car_form = CarForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+      
+    else:
+        new_car_form = CarForm()
+    return render(request, 'new_car.html', {'new_car_form': new_car_form})
